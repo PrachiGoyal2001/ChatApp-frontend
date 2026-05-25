679e50f0d62624b86f5ed912754a2015e74bfffd
PrachiGoyal2001 <prachi23january@gmail.com>
2026-05-25 23:27:57 +0530

    fixing chrome freeze issue in prod

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -28,7 +28,7 @@
           />
         </div>
 
-        <template v-else>
+        <template>
           <div class="profile-hero">
             <q-avatar size="92px" class="profile-avatar">
               {{ profileInitial }}
